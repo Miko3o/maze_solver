facde90058cb02d48f22bff69aa9e8d8a2bd6fdb
Mario Munoz <mikomunoz459@gmail.com>
2022-12-31 20:43:19 -0800

    Mergesort somewhat works now Radix and Selection sort finished still some bugs to fix: - weird behavior when clicking on the maze when done with sort - when drawing maze, side square thing happens - undo/redo functionality still screwed up

--- part_3_b/Controller/Strategies/SelectionSort.py
+++ part_3_b/Controller/Strategies/SelectionSort.py
@@ -8,4 +8,42 @@ class SelectionSort(AbstractStrategy):
     super().__init__(*args)
     
   def Sort(self, currentGrid, gridSize, currentPastGrids, currentPastGridsIndex):
-    print("wip")
+    wallHeights = super().Sort(currentGrid)
+    clock = pygame.time.Clock()
+
+    #STEP 1: iterate through whole array
+    for i in range(len(wallHeights)):
+      
+      #STEP 2: find minimum
+      minHeight = i
+      for nextIndex in range(i + 1, len(wallHeights)):
+        if wallHeights[minHeight] > wallHeights[nextIndex]:
+          minHeight = nextIndex
+
+      #STEP 3: swap the minimum with the first index
+      self.SwapIndexes(currentGrid, wallHeights, i, minHeight, gridSize, currentPastGrids, currentPastGridsIndex)
+
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+          exit()
+      pygame.display.update()
+      clock.tick(10)
+
+    print(wallHeights)
+
+
+  def SwapIndexes(self, currentGrid, wallHeights, currentIndex, minHeight, gridSize, currentPastGrids, currentPastGridsIndex):
+    #swap list indecies
+    indexToBeSwapped = wallHeights[currentIndex]
+    wallHeights[currentIndex] = wallHeights[minHeight]
+    wallHeights[minHeight] = indexToBeSwapped
+    
+    #swap grid indecies
+    for gridRow in range(len(currentGrid)):
+        gridIndexToBeSwapped = currentGrid[gridRow][currentIndex]
+        currentGrid[gridRow][currentIndex] = currentGrid[gridRow][minHeight]
+        currentGrid[gridRow][minHeight] = gridIndexToBeSwapped
+
+    self.viewManager.modelManager.gridMetaData.ChangeGridData(currentGrid, False, False, False)
+    self.viewManager.uiGrid.Draw(currentGrid, currentPastGrids, 0, 0, go.NOTHING, currentPastGridsIndex)
