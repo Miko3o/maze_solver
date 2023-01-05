26e44bbee57858c29f75b8c33f455b566ee5fd10
Mario Munoz <mikomunoz459@gmail.com>
2023-01-05 03:11:47 -0800

    Firgured out how to mock for tests BubbleSort tests done

--- part_3_b/Controller/Strategies/test_BubbleSort.py
+++ part_3_b/Controller/Strategies/test_BubbleSort.py
@@ -1,34 +1,56 @@
 import pytest
-from unittest.mock import MagicMock, patch
+import pygame
+import unittest
+from View.GameStateENUM import GridObjects as go
+from unittest.mock import MagicMock, patch, Mock
 from Controller.Strategies.BubbleSort import BubbleSort
+#from Controller.Strategies.mock_AbstractStrategy import mock_AbstractStrategy
+
 
 #Sort--------------------------------------------
+class Test_BubbleSort(unittest.TestCase):
+
+  @patch("pygame.display.update")
+  @patch("pygame.event.get")
+  def test_UIIsUpdated_When_SortCalled(self, mock_pygameEventGet, mock_pygameDisplayUpdate):
+    #SETUP-----------------------------------------------
+    #pygame functions to be mocked
+    mock_pygameEventGet.return_value = []
+    mock_pygameDisplayUpdate.return_value = "hi"
+    
+    #arg classes to be mocked
+    viewManager = Mock()
+    gameWindow = Mock()
+
+    #bubblesort object
+    bubbleSort = BubbleSort(viewManager, gameWindow)
+
+    #args to be passed into the sort method
+    N = go.NOTHING
+    W = go.WALL
+    currentGrid = [
+                  [N, N, N, N, N, N, N, N],
+                  [N, N, N, N, W, N, N, N],
+                  [N, N, N, N, W, N, N, N],
+                  [N, W, N, N, W, N, N, N],
+                  [N, W, N, N, W, N, W, N],
+                  [N, W, N, N, W, N, W, N],
+                  [N, W, W, W, W, W, W, W],
+                  [W, W, W, W, W, W, W, W],
+                                          ]
+    gridSize = 8
+    currentPastGrids = []
+    currentPastGridsIndex = 0
 
 
 
-@pytest.mark.xfail
-def WallsAreSorted_When_SortCalled():
-  #setup
-  bubbleSort = BubbleSort()
-  N = "nothing"
-  W = "wall"
-  currentGrid = [
-                [N, N, N, N, N, N, N, N],
-                [N, N, N, N, W, N, N, N],
-                [N, N, N, N, W, N, N, N],
-                [N, W, N, N, W, N, N, N],
-                [N, W, N, N, W, N, W, N],
-                [N, W, N, N, W, N, W, N],
-                [N, W, W, W, W, W, W, W],
-                [W, W, W, W, W, W, W, W],
-                                        ]
-
-  #work
-  currentGrid = bubbleSort.Sort()
+    #WORK--------------------------------------------
+    bubbleSort.Sort(currentGrid, gridSize, currentPastGrids, currentPastGridsIndex)
 
 
-  #assert
-  assert   currentGrid == [
+
+    #ASSERT-------------------------------------------
+    assert currentGrid == [
                           [N, N, N, N, N, N, N, N],
                           [N, N, N, N, N, N, N, W],
                           [N, N, N, N, N, N, N, W],
@@ -38,3 +60,80 @@ def WallsAreSorted_When_SortCalled():
                           [N, W, W, W, W, W, W, W],
                           [W, W, W, W, W, W, W, W],
                                                   ]
+
+
+  #SwapIndexes------------------------------------------
+  def test_WallHeightsIndexesAreSwapped_When_Called(self):
+    #SETUP----------------------------------------------
+    #arg classes to be mocked
+    viewManager = Mock()
+    gameWindow = Mock()
+
+    #bubblesort object
+    bubbleSort = BubbleSort(viewManager, gameWindow)
+
+    #args to be passed into the swap method
+    N = go.NOTHING
+    W = go.WALL
+    currentGrid = [
+                  [N, N, N, N, N, N, N, N],
+                  [N, N, N, N, W, N, N, N],
+                  [N, N, N, N, W, N, N, N],
+                  [N, W, N, N, W, N, N, N],
+                  [N, W, N, N, W, N, W, N],
+                  [N, W, N, N, W, N, W, N],
+                  [N, W, W, W, W, W, W, W],
+                  [W, W, W, W, W, W, W, W],
+                                          ]
+    currentPastGrids = []
+    wallHeights = [1, 5, 2, 2, 7, 2, 4, 2]
+
+    #WORK----------------------------------------------
+    bubbleSort.SwapIndexes(currentGrid, wallHeights, 1, currentPastGrids)
+
+    #ASSERT--------------------------------------------
+    assert wallHeights == [1, 2, 5, 2, 7, 2, 4, 2]
+
+  def test_GridIndexesAreSwapped_When_Called(self):
+    #SETUP----------------------------------------------
+    #arg classes to be mocked
+    viewManager = Mock()
+    gameWindow = Mock()
+
+    #bubblesort object
+    bubbleSort = BubbleSort(viewManager, gameWindow)
+
+    #args to be passed into the swap method
+    N = go.NOTHING
+    W = go.WALL
+    currentGrid = [
+                  [N, N, N, N, N, N, N, N],
+                  [N, N, N, N, W, N, N, N],
+                  [N, N, N, N, W, N, N, N],
+                  [N, W, N, N, W, N, N, N],
+                  [N, W, N, N, W, N, W, N],
+                  [N, W, N, N, W, N, W, N],
+                  [N, W, W, W, W, W, W, W],
+                  [W, W, W, W, W, W, W, W],
+                                          ]
+    currentPastGrids = []
+    wallHeights = [1, 5, 2, 2, 7, 2, 4, 2]
+
+    #WORK----------------------------------------------
+    bubbleSort.SwapIndexes(currentGrid, wallHeights, 1, currentPastGrids)
+
+    #ASSERT--------------------------------------------
+    assert currentGrid == [
+                          [N, N, N, N, N, N, N, N],
+                          [N, N, N, N, W, N, N, N],
+                          [N, N, N, N, W, N, N, N],
+                          [N, N, W, N, W, N, N, N],
+                          [N, N, W, N, W, N, W, N],
+                          [N, N, W, N, W, N, W, N],
+                          [N, W, W, W, W, W, W, W],
+                          [W, W, W, W, W, W, W, W],
+                                                  ]
+
+
+if __name__ == "__main__":
+  unittest.main()
