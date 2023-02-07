5ef84fc4bf619fa3c28b70cfd95112f89535569f
Mario Munoz <mikomunoz459@gmail.com>
2023-02-06 20:58:42 -0800

    Delete part_1 directory

--- part_1/What_Function.py
+++
@@ -1,9 +0,0 @@
-from GameStateENUM import BattleState as bs
-
-def Not_In_Battle_What(player, item, weapon, armor, gameState, gameManager):
-  print("What?")
-  return bs.NOT_IN_BATTLE
-
-def In_Battle_What(player, enemy, item, gameState, enemy_chosen, gameManager):
-  print("What?")
-  return bs.IN_BATTLE
